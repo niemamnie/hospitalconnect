06f525ba111b056de4d5aff0da8f2379c4d04ec0
niemamnie <niemamnieeee@gmail.com>
2020-03-21 11:39:41 +0100

    init project

---
+++ src/main/java/de/hackathon/hospitalconnect/HospitalconnectApplication.java
@@ -0,0 +1,13 @@
+package de.hackathon.hospitalconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalconnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalconnectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/hackathon/hospitalconnect/HospitalconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hackathon.hospitalconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalconnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
